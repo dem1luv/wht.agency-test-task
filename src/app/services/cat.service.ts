af89e13131ceb06accf6832780788ca31ca6a993
Dmytro Demyanenko <dmytro.demjanenko@gmail.com>
2023-07-30 16:36:35 +0300

    add api key

--- src/app/services/cat.service.ts
+++ src/app/services/cat.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { CAT_API_KEY } from '../app.constant';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class CatService {
   constructor(private readonly http: HttpClient) { }
 
   getList(limit: number) {
-    return this.http.get<any[]>(`https://api.thecatapi.com/v1/images/search?limit=${limit}`);
+    return this.http.get<any[]>(`https://api.thecatapi.com/v1/images/search?api_key=${CAT_API_KEY}&limit=${limit}`);
   }
 }
