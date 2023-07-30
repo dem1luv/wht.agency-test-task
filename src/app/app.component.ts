d015f4a5ecdb51fe6f498ce767388270243263c9
Dmytro Demyanenko <dmytro.demjanenko@gmail.com>
2023-07-30 13:04:30 +0300

    add responsive grid list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'wht.agency-test-task';
+export class AppComponent implements OnInit {
+  gridCols: number = 6;
+
+  ngOnInit() {
+    this.resizeGridList(window.innerWidth);
+  }
+
+  resizeGridList(innerWidth: number) {
+    if (innerWidth > 1200) {
+      this.gridCols = 6;
+    } else if (innerWidth > 900) {
+      this.gridCols = 5;
+    } else if (innerWidth > 600) {
+      this.gridCols = 4;
+    } else if (innerWidth > 400) {
+      this.gridCols = 3;
+    } else {
+      this.gridCols = 2;
+    }
+  }
+
+  onGridResize(event: any) {
+    this.resizeGridList(event.target.innerWidth);
+  }
 }
