abc52050f8d1fa488c7e6884d870358dd28c4d02
Dmytro Demyanenko <dmytro.demjanenko@gmail.com>
2023-08-01 08:34:36 +0300

    add cancellation of a previous request if the search form is changed

--- src/app/services/cat.service.ts
+++ src/app/services/cat.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { CAT_API_KEY } from '../app.constant';
 import { ICat } from '../types/common/cat.interface';
 import { IBreed } from '../types/common/breed.interface';
+import { BehaviorSubject, of, retry, Subject, takeUntil, tap } from 'rxjs';
 
 const apiUrl = `https://api.thecatapi.com/v1`;
 
@@ -10,12 +11,29 @@ const apiUrl = `https://api.thecatapi.com/v1`;
   providedIn: 'root'
 })
 export class CatService {
+  getListLastTimestamp: number = 0;
 
   constructor(private readonly http: HttpClient) { }
 
   getList(breedIds: string[], limit: number) {
+    const timestamp = Date.now();
+    this.getListLastTimestamp = timestamp;
+    const cancelRequest = new Subject<void>();
+
     const breedIdsQuery = breedIds ? breedIds.join(',') : '';
-    return this.http.get<ICat[]>(`${apiUrl}/images/search?api_key=${CAT_API_KEY}&breed_ids=${breedIdsQuery}&limit=${limit}`);
+    const url = `${apiUrl}/images/search?api_key=${CAT_API_KEY}&breed_ids=${breedIdsQuery}&limit=${limit}`;
+
+    return this.http
+      .get<ICat[]>(url)
+      .pipe(
+        tap(() => {
+          if (timestamp < this.getListLastTimestamp) {
+            cancelRequest.next();
+            cancelRequest.complete();
+          }
+        }),
+        takeUntil(cancelRequest),
+      );
   }
 
   getBreedList() {
